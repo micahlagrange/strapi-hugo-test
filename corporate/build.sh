920041f0a555ba48ef267517cf62c028ce1f38d8
micah turner <micah@webscale.com>
2021-04-27 20:24:02 +0000

    update CI build script

---
+++ corporate/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+cd /home/ubuntu/strapi-hugo-test/corporate/
+
+hugo -D --ignoreCache
+
+aws s3 rm --recursive s3://webscale-strapi-generated/
+aws s3 sync ./public/ s3://webscale-strapi-generated/
+
